f720817d4e6e7a80493b8f0ba5601e03273bcc50
dmlb2000 <dmlb2000@gmail.com>
2015-02-11 15:47:11 -0800

    forgot to specify the path

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,10 +10,12 @@ node.default['utilite']['ttymxc1']['baudrate'] = 115200
 node.default['utilite']['ttymxc3']['baudrate'] = 115200
 template "ttymxc3" do
   source "serial.erb"
+  path "/etc/init/ttymxc3.conf"
   variables :name => "ttymxc3"
 end
 template "ttymxc1" do
   source "serial.erb"
+  path "/etc/init/ttymxc1.conf"
   variables :name => "ttymxc1"
 end
 
