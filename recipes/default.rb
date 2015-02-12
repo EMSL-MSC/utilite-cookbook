28d106caff2267b48e757e5d98a2b9aee2279965
dmlb2000 <dmlb2000@gmail.com>
2015-02-12 14:15:17 -0800

    lets do some more basic stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,17 +24,6 @@ package "e2fsprogs"
 package "openssh-server"
 package "dhcpcd5"
 
-node.default['filesystems']['BOOT']['device'] = "/dev/disk/by-label/BOOT"
-node.default['filesystems']['BOOT']['fstype'] = "vfat"
-node.default['filesystems']['BOOT']['owner'] = "root"
-node.default['filesystems']['BOOT']['group'] = "root"
-node.default['filesystems']['BOOT']['mode'] = "0755"
-node.default['filesystems']['BOOT']['mount'] = "/boot"
-node.default['filesystems']['BOOT']['nomkfs'] = true
-node.default['filesystems']['BOOT']['nomount'] = true
-
-include_recipe "filesystem"
-
 user "root" do
   password '$1$1ABryTOX$A2b.tXEE9fxKiwlLnCFks1'
 end
